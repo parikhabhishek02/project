443806b530ec76c0980909ad48709dae690ecfa0
parikhabhishek02 <40873007+parikhabhishek02@users.noreply.github.com>
2019-08-23 13:06:04 -0400

    Update BB_LPC_code2.c

--- MCU MOTOR code/lpcxpresso54114_BB_LPC_code2/source/BB_LPC_code2.c
+++ MCU MOTOR code/lpcxpresso54114_BB_LPC_code2/source/BB_LPC_code2.c
@@ -13,8 +13,6 @@
 #include "clock_config.h"
 #include "LPC54114_cm4.h"
 #include "fsl_debug_console.h"
-
-
 /* TODO: insert other include files here. */
 
 /* FreeRTOS kernel includes. */
@@ -26,42 +24,53 @@
 #include "fsl_usart_freertos.h"
 #include "fsl_usart.h"
 
+#include <stdbool.h>
 #include "fsl_ctimer.h"
-
 /* TODO: insert other definitions and declarations here. */
 
-#define CTIMER CTIMER0                 /* Timer 0 */
-#define LM0 kCTIMER_Match_0			//	J1[19]
-#define RM0 kCTIMER_Match_1			//  J2[18]
-#define LM1 kCTIMER_Match_2			//  J1[16]
-#define RM1 kCTIMER_Match_0			//  J2[17]
+/* Freescale includes. */
+#include "fsl_device_registers.h"
 
 
-#define DEMO_USART USART0
+//pin configurations
+#define CTIMER CTIMER0		// Timer 0
+#define CTIMER_CLK_FREQ CLOCK_GetFreq(kCLOCK_AsyncApbClk)
+
+#define LEFT_MOTOR1 kCTIMER_Match_3		//J2[15]
+#define LEFT_MOTOR2 kCTIMER_Match_1		//J2[18]
+
+
+#define RIGHT_MOTOR1 kCTIMER_Match_2		//J1[16]
+#define RIGHT_MOTOR2 kCTIMER_Match_0		//J1[19]
+
+
+
+#define DEMO_USART USART0		//uart module communication
 #define DEMO_USART_IRQHandler FLEXCOMM0_IRQHandler
 #define DEMO_USART_IRQn FLEXCOMM0_IRQn
-
-
 /* Task priorities. */
-#define uart_task_PRIORITY (configMAX_PRIORITIES - 1)
+#define uart_task_PRIORITY (configMAX_PRIORITIES - 1)		//set priority for queue to task
 #define USART_NVIC_PRIO 5
-
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
 static void uart_task(void *pvParameters);
 static void Drive_task(void *pvParameters);
 
-void MotorsSetup();
+
+void Motors();
 void Forward();
-void Turn_Left();
-void Turn_Right();
+void Left();
+void Right();
 void Stop();
-void Reverse();
+
+
 
 uint8_t background_buffer[32];
-uint8_t recv_buffer[1];
+uint8_t recv_buffer[1];			//receiver from bbb
 
 usart_rtos_handle_t handle;
-struct _usart_handle t_handle;
-
+struct _usart_handle t_handle;		//uart configuration to signal handling
 
 struct rtos_usart_config usart_config = {
     .baudrate    = 115200,
@@ -71,111 +80,101 @@ struct rtos_usart_config usart_config = {
     .buffer_size = sizeof(background_buffer),
 };
 
-QueueHandle_t xQueue1;
+QueueHandle_t xQueue1;			//initializing queue for task
 
+/*
+ * @brief   Application entry point.
+ */
+int main(void) {
 
-int main(void)
-
-{
-
-	CLOCK_AttachClk(BOARD_DEBUG_UART_CLK_ATTACH);
-	SYSCON->ASYNCAPBCTRL = 1;
-	//CLOCK_AttachClk(kFRO12M_to_ASYNC_APB);
-
-	// ctimer_config_t config;
-	// uint32_t srcClock_Hz;
-
-
+  	/* Init board hardware. */
     BOARD_InitBootPins();
     BOARD_InitBootClocks();
     BOARD_InitBootPeripherals();
+  	/* Init FSL debug console. */
+    BOARD_InitDebugConsole();
 
 
-   // srcClock_Hz = CLOCK_GetFreq(kCLOCK_BusClk);
+  // MotorsSetup();
 
-  //  CTIMER_GetDefaultConfig(&config);
-    //CTIMER_Init(CTIMER, &config);
-   // CTIMER_Init(CTIMER1, &config);
-   // CTIMER_SetupPwm(CTIMER,LM0,50,20000,srcClock_Hz,0);
-   // CTIMER_SetupPwm(CTIMER,LM1,50,20000,srcClock_Hz,0);
+    ctimer_config_t config;
+    		 uint32_t srcClock_Hz;
+    		 srcClock_Hz = CLOCK_GetFreq(kCLOCK_BusClk);
 
 
 
-   // CTIMER_SetupPwm(CTIMER,RM0,50,20000,srcClock_Hz,0);
-   // CTIMER_SetupPwm(CTIMER1,RM1,50,20000,srcClock_Hz,0);
-
-    //CTIMER_StartTimer(CTIMER);
-    //CTIMER_StartTimer(CTIMER1);
+    	    CTIMER_GetDefaultConfig(&config);
 
 
-   // CTIMER_UpdatePwmDutycycle(CTIMER,LM0,75);
-   // CTIMER_UpdatePwmDutycycle(CTIMER,LM1,75);
+    	    CTIMER_Init(CTIMER, &config);		//configure ctimer pwm signals
 
-    //CTIMER_UpdatePwmDutycycle(CTIMER,RM0,75);
-    //CTIMER_UpdatePwmDutycycle(CTIMER1,RM1,75);
+    	    CTIMER_SetupPwm(CTIMER,LEFT_MOTOR1,1,20000,srcClock_Hz,NULL);
+    	    CTIMER_SetupPwm(CTIMER,LEFT_MOTOR2,1,20000,srcClock_Hz,NULL);
+    	    CTIMER_SetupPwm(CTIMER,RIGHT_MOTOR1,1,20000,srcClock_Hz,NULL);
+    	    CTIMER_SetupPwm(CTIMER,RIGHT_MOTOR2,1,20000,srcClock_Hz,NULL);
+    	    CTIMER_StartTimer(CTIMER);
 
-    MotorsSetup();
 
-    if (xQueue1 == NULL)
-    {
+    	  	CTIMER_UpdatePwmDutycycle(CTIMER, LEFT_MOTOR1, 70);		//configure ctimer pwm dutycycle
+    	  	CTIMER_UpdatePwmDutycycle(CTIMER, LEFT_MOTOR2, 1);
+
+
+    	  	CTIMER_UpdatePwmDutycycle(CTIMER, RIGHT_MOTOR1, 70);
+    	  	CTIMER_UpdatePwmDutycycle(CTIMER, RIGHT_MOTOR2, 1);
+
+if (xQueue1 == NULL)				//initializing queue
+{
     xQueue1=xQueueCreate(1,sizeof(char));
-
-
     if (xTaskCreate(uart_task, "Uart_task", configMINIMAL_STACK_SIZE + 10, NULL, 2, NULL) != pdPASS)
-         {
-             PRINTF("Task creation failed!.\r\n");
-             while (1)
-                 ;
-         }
-
-
+      {
+          PRINTF("Task creation failed!.\r\n");
+          while (1)
+              ;
+      }
     if (xTaskCreate(Drive_task, "Robot_driving_task", configMINIMAL_STACK_SIZE + 10, NULL,2, NULL) != pdPASS)
-                {
-                    PRINTF("Task creation failed!.\r\n");
-                    while (1)
-                        ;
-                }
+             {
+                 PRINTF("Task creation failed!.\r\n");
+                 while (1)
+                     ;
+             }
 
-         vTaskStartScheduler();
-         for (;;)
-             ;
-    }
-
+      vTaskStartScheduler();
+      for (;;)
+         ;
 }
+  }
+
+  /*!
+   * @brief Task responsible for loopback.
+   */
+  static void uart_task(void *pvParameters)
+  {
+      char send;
+      size_t n            = 0;
+      usart_config.srcclk = BOARD_DEBUG_UART_CLK_FREQ;
+      usart_config.base   = DEMO_USART;
+
+      NVIC_SetPriority(DEMO_USART_IRQn, USART_NVIC_PRIO);
+
+      USART_RTOS_Init(&handle, &t_handle, &usart_config);
+
+      /* Receive user input and send it back to terminal. */
+      while(1)
+      {
+          USART_RTOS_Receive(&handle, recv_buffer, sizeof(recv_buffer), &n);		//receiver for uart signal from bbb
+          if (n > 0)
+          {
+        		send=recv_buffer[0];
+        		xQueueSend(xQueue1,&send,10);
+          }
+      }
+
+  }
 
 
 
-
-
-
-static void uart_task(void *pvParameters)
- {
-     char send;
-     size_t n            = 0;
-     usart_config.srcclk = BOARD_DEBUG_UART_CLK_FREQ;
-     usart_config.base   = DEMO_USART;
-
-     NVIC_SetPriority(DEMO_USART_IRQn, USART_NVIC_PRIO);
-
-     USART_RTOS_Init(&handle, &t_handle, &usart_config);
-
-     /* Receive user input and send it back to terminal. */
-     while(1)
-     {
-         USART_RTOS_Receive(&handle, recv_buffer, sizeof(recv_buffer), &n);
-         if (n > 0)
-         {
-       		send=recv_buffer[0];
-       		xQueueSend(xQueue1,&send,10);
-         }
-     }
-
- }
-
-
-
- static void Drive_task(void *pvParameters)
- {
+  static void Drive_task(void *pvParameters)		//driver task for getting signal
+  {
 	  char recv;
 
 	  	while(1){
@@ -183,7 +182,7 @@ static void uart_task(void *pvParameters)
 	  		if(recv=='F')
 	  		{
 	  			Forward();
-	  			printf("forward\n");
+	  			printf("moving\n");
 	  			recv='n';
 	  		}
 	  		else if(recv=='L')
@@ -200,26 +199,24 @@ static void uart_task(void *pvParameters)
 	  		}
 	  		else if(recv=='S')
 	  		{
+
 	  			Stop();
 	  			printf("stop");
 	  			recv='n';
 
 	  		}
-	  		else if(recv=='B')
-	  		{
-	  			 Reverse();
-	  			 printf("reverse\n");
-	  			recv='n';
-	  		}
+
+
+
 
 
 	  	}
- }
+  }
 
 
 
- void MotorsSetup()
- {
+  void Motors()
+  {
 		ctimer_config_t config;
 		 uint32_t srcClock_Hz;
 		 srcClock_Hz = CLOCK_GetFreq(kCLOCK_BusClk);
@@ -230,69 +227,61 @@ static void uart_task(void *pvParameters)
 
 
 	    CTIMER_Init(CTIMER, &config);
-	    CTIMER_Init(CTIMER1, &config);
-	    CTIMER_SetupPwm(CTIMER,LM0,0,20000,srcClock_Hz,NULL);
-	    CTIMER_SetupPwm(CTIMER,LM1,0,20000,srcClock_Hz,NULL);
-	    CTIMER_SetupPwm(CTIMER,RM0,0,20000,srcClock_Hz,NULL);
-	    CTIMER_SetupPwm(CTIMER1,RM1,0,20000,srcClock_Hz,NULL);
+
+	    CTIMER_SetupPwm(CTIMER,LEFT_MOTOR1,1,20000,srcClock_Hz,NULL);
+	    CTIMER_SetupPwm(CTIMER,LEFT_MOTOR2,1,20000,srcClock_Hz,NULL);
+	    CTIMER_SetupPwm(CTIMER,RIGHT_MOTOR1,1,20000,srcClock_Hz,NULL);
+	    CTIMER_SetupPwm(CTIMER,RIGHT_MOTOR2,1,20000,srcClock_Hz,NULL);
 	    CTIMER_StartTimer(CTIMER);
-	    CTIMER_StartTimer(CTIMER1);
- }
+  }
 
 
- void Forward()
- {
- 	CTIMER_UpdatePwmDutycycle(CTIMER, LM0, 80);
- 	CTIMER_UpdatePwmDutycycle(CTIMER, LM1, 0);
+  void Forward()			//task for forward
+  {
+  	CTIMER_UpdatePwmDutycycle(CTIMER, LEFT_MOTOR1, 70);
+  	CTIMER_UpdatePwmDutycycle(CTIMER, LEFT_MOTOR2, 1);
 
 
- 	CTIMER_UpdatePwmDutycycle(CTIMER, RM0, 80);
- 	CTIMER_UpdatePwmDutycycle(CTIMER1, RM1, 0);
+  	CTIMER_UpdatePwmDutycycle(CTIMER, RIGHT_MOTOR1, 70);
+  	CTIMER_UpdatePwmDutycycle(CTIMER, RIGHT_MOTOR2, 1);
 
- }
-
-
- void Turn_Left()
- {
- 	CTIMER_UpdatePwmDutycycle(CTIMER, LM0, 0);
- 	CTIMER_UpdatePwmDutycycle(CTIMER, LM1, 0);
-
-
- 	CTIMER_UpdatePwmDutycycle(CTIMER, RM0,100);
- 	CTIMER_UpdatePwmDutycycle(CTIMER1, RM1,0);
- }
- void Turn_Right()
- {
- 	CTIMER_UpdatePwmDutycycle(CTIMER, LM0, 100);
- 	CTIMER_UpdatePwmDutycycle(CTIMER, LM1, 0);
-
-
- 	CTIMER_UpdatePwmDutycycle(CTIMER, RM0, 0);
- 	CTIMER_UpdatePwmDutycycle(CTIMER1, RM1, 0);
-
- }
-
- /* Api is used to stop the robot */
-
- void Stop(){
- 	CTIMER_UpdatePwmDutycycle(CTIMER, LM0,0);
- 	CTIMER_UpdatePwmDutycycle(CTIMER, LM1,0);
-
-
- 	CTIMER_UpdatePwmDutycycle(CTIMER, RM0,0);
- 	CTIMER_UpdatePwmDutycycle(CTIMER1,RM1,0);
- }
+  }
 
 
 
- void Reverse(){
+  void Left()		//task for left
+  {
+  	CTIMER_UpdatePwmDutycycle(CTIMER, LEFT_MOTOR1, 70);
+  	CTIMER_UpdatePwmDutycycle(CTIMER, LEFT_MOTOR2, 1);
 
- 	CTIMER_UpdatePwmDutycycle(CTIMER, LM0, 0);
- 	CTIMER_UpdatePwmDutycycle(CTIMER, LM1, 100);
+
+  	CTIMER_UpdatePwmDutycycle(CTIMER, RIGHT_MOTOR1, 1);
+  	CTIMER_UpdatePwmDutycycle(CTIMER, RIGHT_MOTOR2, 1);
+  }
+  void Right()		//task for right
+  {
+  	CTIMER_UpdatePwmDutycycle(CTIMER, LEFT_MOTOR1, 1);
+  	CTIMER_UpdatePwmDutycycle(CTIMER, LEFT_MOTOR2, 1);
 
 
- 	CTIMER_UpdatePwmDutycycle(CTIMER, RM0, 0);
- 	CTIMER_UpdatePwmDutycycle(CTIMER1,RM1, 100);
+  	CTIMER_UpdatePwmDutycycle(CTIMER, RIGHT_MOTOR1, 70);
+  	CTIMER_UpdatePwmDutycycle(CTIMER, RIGHT_MOTOR2, 1);
 
- }
+  }
+
+  /* Api is used to stop the robot */
+
+  void Stop()			//task for stop
+  {
+  	CTIMER_UpdatePwmDutycycle(CTIMER, LEFT_MOTOR1, 1);
+  	CTIMER_UpdatePwmDutycycle(CTIMER, LEFT_MOTOR2, 1);
+
+
+  	CTIMER_UpdatePwmDutycycle(CTIMER, RIGHT_MOTOR1, 1);
+  	CTIMER_UpdatePwmDutycycle(CTIMER, RIGHT_MOTOR2, 1);
+  }
+
+
+
+
 
